91f50f56dbb3f6f5a1cebefd6f1d4864f9be3f57
kevina71 <127604559+kevina71@users.noreply.github.com>
2023-04-01 14:37:05 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculadora
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-u1hzrx)
---
+++ src/main.ts
@@ -0,0 +1,54 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+})
+export class App {
+  name = 'Angular';
+  display = '0';
+  primerValor: number | null = null;
+  action: string | null = null;
+  numClick(valor) {
+    if (this.display === '0') {
+      this.display = valor.toString();
+    } else {
+      this.display = `${this.display}${valor}`;
+    }
+  }
+  operador(tipo) {
+    this.primerValor = parseFloat(this.display);
+    this.action = tipo;
+    this.display = ' ';
+  }
+  reset() {
+    this.display = '0';
+    this.primerValor = null;
+    this.action = null;
+  }
+  calcular() {
+    let a = this.primerValor;
+    let b = parseFloat(this.display);
+
+    switch (this.action) {
+      case 's':
+        this.display = (a + b).toString();
+        break;
+      case 'r':
+        this.display = (a - b).toString();
+        break;
+      case 'm':
+        this.display = (a * b).toString();
+        break;
+      case 'd':
+        this.display = (a / b).toString();
+    }
+  }
+}
+
+bootstrapApplication(App);
